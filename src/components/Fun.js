593bb9ca546c482e9a3c53d5a20be3851fb44022
americanman <snowboardracer7@gmail.com>
2018-08-06 01:38:38 -0400

    Test props type of Fun component 'oneOfType' method

--- src/components/Fun.js
+++ src/components/Fun.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Fun extends Component {
   render() {
@@ -6,6 +7,12 @@ class Fun extends Component {
       <h2 onClick={this.props.addFun}>{this.props.fun}</h2>
     )
   }
+}
+
+Fun.propTypes = {
+  addFun: PropTypes.func,
+  //Test will be passed with any type of props that listed below
+  fun: PropTypes.oneOfType([PropTypes.string, PropTypes.func])
 }
 
 export default Fun
