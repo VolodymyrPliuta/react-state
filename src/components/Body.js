f6fbf21a341bcfb78e5dbcae966296fe1ee05a15
americanman <snowboardracer7@gmail.com>
2018-11-26 06:25:55 -0500

    Call three functions on click

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,14 +2,19 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 class Body extends React.Component {
-  boom =() => {
+   boom = () => {
     console.log('number')
+  }
+
+  callThree = () => {
+    this.props.removeConf();
+    this.props.changeColor();
+    this.boom();
   }
   render() {
     return (
       <div>
-        <p onClick={this.props.changeColor}
-          onClick={this.props.removeConf}>to pass as a property, We use this.props.NameOfTheProperty {this.props.conference}</p>
+        <p onClick={this.callTwo}>to pass as a property, We use this.props.NameOfTheProperty {this.props.conference}</p>
         <p className="rum" onMouseEnter={this.props.removeP}>Book. another stare the nest conf is {this.props.hangout}</p>
         <p>pass in {this.props.conference}</p>
         <p><Link to='/route'>Route</Link></p>
