661b599257edc1cb944f8880e4696dce5cce16d3
americanman <snowboardracer7@gmail.com>
2018-11-26 06:37:04 -0500

    Implement callback to call multiple functions whem event occures

--- src/components/Body.js
+++ src/components/Body.js
@@ -11,10 +11,15 @@ class Body extends React.Component {
     this.props.changeColor();
     this.boom();
   }
+
   render() {
     return (
       <div>
-        <p onClick={this.callTwo}>to pass as a property, We use this.props.NameOfTheProperty {this.props.conference}</p>
+       {/* we can call multiple functions combining them together
+        <p onClick={this.callThree}>to pass as a property, We use this.props.NameOfTheProperty {this.props.conference}</p>
+
+        we also can call multiple funcions using callback, in this case we don't have to combine them at one function */}
+        <p onClick={() => {this.props.removeConf(); this.props.changeColor(); this.boom()}}>to pass as a property, We use this.props.NameOfTheProperty {this.props.conference}</p>
         <p className="rum" onMouseEnter={this.props.removeP}>Book. another stare the nest conf is {this.props.hangout}</p>
         <p>pass in {this.props.conference}</p>
         <p><Link to='/route'>Route</Link></p>
